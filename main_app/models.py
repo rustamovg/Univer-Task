68aa92eaafae4df1c41b717b110cebcdfd4105f5
Rustam <20.06RustamRustamov@gmail.com>
2025-03-26 14:52:23 +0500

    User and Question models created

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField("Full name", max_length=250)
+    email = models.EmailField("Email", unique=True)
+    phone_number = PhoneNumberField("Phone number", null=False, blank=False, unique=True, region="UZ")
+
+    def __str__(self):
+        return self.name
+    
+class Questions(models.Model):
+    name = models.CharField("Full name", max_length=250)
+    email = models.EmailField("Email", unique=True)
+    message = models.TextField("Question")
+
+    def __str__(self):
+        return self.name
