7c76a73bb6337e9093e7706b8cd23672e1e047e9
Rustam Rustamov <20.06rustamrustamov@gmail.com>
2025-03-28 15:11:49 +0500

    template translated and notification have been added

--- main_app/views.py
+++ main_app/views.py
@@ -2,23 +2,29 @@ from django.shortcuts import render
 from .forms import UserForm, QuestionForm
 
 def home_page(request):
+    message = None
     if request.method == "POST":
         user_form = UserForm(request.POST)
         question_form = QuestionForm(request.POST)
 
         if user_form.is_valid():
             user_form.save()
-            return render(request, "success.html")
+            message = "The registration request has been successfully sent"
 
-        if question_form.is_valid():
+        elif question_form.is_valid():
             question_form.save()
-            return render(request, "success.html")
+            message = "The question has been successfully sent"
+        else:
+            message = "There were errors in your form submission. Please check your inputs"
 
-        print(user_form.errors)  # Print form errors if validation fails
+        print(user_form.errors)
         print(question_form.errors)
 
     else:
         user_form = UserForm()
         question_form = QuestionForm()
 
-    return render(request, "index.html", {"user_form": user_form, "question_form": question_form})
+    return render(request, "index.html", 
+                  {"user_form": user_form,
+                   "question_form": question_form,
+                   "message": message})
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -49,18 +49,6 @@
     });
 
 
-    /* Countdown Timer - The Final Countdown */
-	$('#clock').countdown('2020/12/27 08:50:56') /* change here your "countdown to" date */
-	.on('update.countdown', function(event) {
-		var format = '<span class="counter-number">%D<br><span class="timer-text">Days</span></span><span class="counter-number">%H<br><span class="timer-text">Hours</span></span><span class="counter-number">%M<br><span class="timer-text">Minutes</span></span><span class="counter-number">%S<br><span class="timer-text">Seconds</span></span>';
-		$(this).html(event.strftime(format));
-	})
-	.on('finish.countdown', function(event) {
-	$(this).html('This offer has expired!')
-		.parent().addClass('disabled');
-    });
-
-
     /* Image Slider 2 - Swiper */
     var imageSliderOne = new Swiper('.image-slider-1', {
         autoplay: {
